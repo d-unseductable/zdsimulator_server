c518bd82be97c51f973e64a804430452995952a8
Dmitry Gritsay <unseductable@gmail.com>
2014-07-08 01:25:02 +0300

    Describe the structures that are sent over the network

--- src/NetStruct.h
+++ src/NetStruct.h
@@ -3,14 +3,17 @@
 
 #include "Types.h"
 
+/*
+ * TNConnect is sent by client to server on the connection stage
+ */
 struct TNConnect
 {
-  ushort  itsTrainNum;
-  uchar   itsDirection;
-  ushort  itsSigNum;
-  ushort  itsTrackId;
-  ushort  itsLocoType;
-  uchar   itsCarNum;
+  ushort  itsTrainNum;  // Train number
+  uchar   itsDirection; // Train direction
+  ushort  itsSigNum;    // Number of signals on the client's route
+  ushort  itsTrackId;   // Starting track id
+  ushort  itsLocoType;  // Locomotive type
+  uchar   itsCarNum;    // Number of cars
 };
 
 struct TNConnectAns
@@ -19,34 +22,47 @@ struct TNConnectAns
   uchar Code;
 };
 
+/*
+ * TNCoords is sent by client to server to describe client's train attributes
+ */
 struct TNCoords
 {
-  ushort  itsTrainNum;
-  ushort  itsHeadTrackId;
-  ushort  itsTailTrackId;
-  float   itsSpeed;
+  ushort  itsTrainNum;    // Train number
+  ushort  itsHeadTrackId; // Train's head track id
+  ushort  itsTailTrackId; // Train's tails track id
+  float   itsSpeed;       // Current speed
 };
 
+/*
+ * TNSvetofor is sent by server to client to describe current state
+ * of a signal.
+ * Usually server sends all the singals' states array
+ */
 struct TNSvetofor
 {
-  ushort  itsId;
-  uchar   itsValue;
+  ushort  itsId;    // Signal id
+  uchar   itsValue; // Signal code
 };
 
-struct TVisibles
-{
-  TTrain  *itsTrain[2];
-};
-
+/*
+ * TNTraf is sent by server to client to describe the traffic visible
+ * to the client
+*/
 struct TNTraf
 {
-  ushort  itsTrainNum;
-  ushort  itsLocoType;
-  ushort  itsHeadTrackId;
-  float   itsSpeed;
-  uchar   itsCarNum;
+  ushort  itsTrainNum;    // Traffic train's number
+  ushort  itsLocoType;    // Locomotive type
+  ushort  itsHeadTrackId; // Head track id
+  float   itsSpeed;       // Current speed
+  uchar   itsCarNum;      // Number of cars
+};
+
+/*
+ * TVisibles describes a pair of trains that are visible to each other
+ */
+struct TVisibles
+{
+  TTrain  *itsTrain[2]; // A pair of trains
 };
 
 #endif
-
-
