ee9b3a545e926a007b6ff6d8bca30c3f27c009b1
Dmitry Gritsay <unseductable@gmail.com>
2014-07-08 23:06:41 +0300

    Describe signal class

--- src/Svetofor.h
+++ src/Svetofor.h
@@ -6,6 +6,9 @@
 enum svType { ab_line, ab_entr, ab_exit, ab_rout };
 enum svValue { svRed = 1, svYellow, svGreenY, svGreen, svYY = 20, svYbY, svYb };
 
+/*
+ * TSvetofor describes a signal on the route (should be renamed)
+ */
 class TSvetofor
 {
 public:
@@ -33,15 +36,15 @@ public:
   const char  *GetName() const;
 
 private:
-  ushort  itsId;
-  char    itsAttr;
-  char    itsName[7];
-  ushort  itsTrackId;
-  uchar   itsType;
-  uchar   itsValue;
-  bool    itsAutoOpen;
-  bool    itsForceOpen;
-  bool    itsGone;
+  ushort  itsId;        // Id
+  char    itsAttr;      // Attributte (like 'Y' for yellow blinking signal)
+  char    itsName[7];   // A literal
+  ushort  itsTrackId;   // Track id
+  uchar   itsType;      // Type (see `svType` enum)
+  uchar   itsValue;     // Value (see `svValue` enum)
+  bool    itsAutoOpen;  // Auto open signal after train pass
+  bool    itsForceOpen; // Force open
+  bool    itsGone;      // Has train passed the signal?
 };
 
 #endif
